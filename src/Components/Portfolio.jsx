0b4781f6d3030f776e08485f8f6b838e0ee6ab0d
dhruvin <dhruvin.200410116086@gmail.com>
2023-07-26 20:06:21 +0530

    animated portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useState,  } from 'react';
+import React from 'react';
 import './Portfolio.css';
 
 import portfolio1 from '../assets/portfolio1.webp'
@@ -6,39 +6,21 @@ import portfolio2 from '../assets/portfolio2.webp'
 import portfolio3 from '../assets/portfolio3.webp'
 
 const Carousel = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const totalSlides = 4; 
-    let isMobile= window.innerWidth<1024;
-
-  const handlePrevSlide = () => {
-    // document.querySelectorAll('.myimg').style.transform='translateX(-10vw)'
-    setCurrentSlide((prevSlide) => (prevSlide === 0 ? totalSlides-1 : prevSlide - 1));
-    
-};
-
-const handleNextSlide = () => {
-    // document.querySelectorAll('.myimg').style.transform='translateX(10vw)'
-    setCurrentSlide((prevSlide) => (prevSlide === totalSlides-1 ? 0 : prevSlide + 1));
-    
-   
-  };
-  // const disablePrevButton = currentSlide === 0;
-  // const disableNextButton =(isMobile? currentSlide === totalSlides-1:currentSlide-2)
   return (
-    <div className="carousel">
-        <h1>My Portfolio</h1>
-      <div className="slider"  style={{
-          transform: `translateX(-${currentSlide * (isMobile ? 100 : 100/3)}%)`,
-        }}>
-        <div className="slide">
-        <img className='myimg'  src={portfolio1} alt=""  />
-        <img className='myimg'  src={portfolio2} alt=""  />
-        <img className='myimg'  src={portfolio3} alt=""  />
-        <img className='myimg'  src={portfolio1} alt=""  />
+    <div className='portfolio-section'>
+      <h1>My Portfolio</h1>
+      <div className="images">
+        <div className="image-slide">
+          <a href="/"><img src={portfolio1} alt="" /></a>
+          <a href="/"><img src={portfolio2} alt="" /></a>
+          <a href="/"><img src={portfolio3} alt="" /></a>
+        </div>
+        <div className="image-slide">
+          <a href="/"><img src={portfolio1} alt="" /></a>
+          <a href="/"><img src={portfolio2} alt="" /></a>
+          <a href="/"><img src={portfolio3} alt="" /></a>
         </div>
       </div>
-          <button    className="arrow-button left" onClick={handlePrevSlide}  > &lt;</button>
-          <button    className="arrow-button right" onClick={handleNextSlide}  >  &gt;</button>
     </div>
   );
 };
